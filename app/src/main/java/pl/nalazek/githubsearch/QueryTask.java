b9a36f64beadb7afe41279e4c70487c6a69a2cd6
Daniel Nalazek <d.nlzk@o2.pl>
2016-12-27 18:31:49 +0100

    finished query-response handling

--- app/src/main/java/pl/nalazek/githubsearch/QueryTask.java
+++ app/src/main/java/pl/nalazek/githubsearch/QueryTask.java
@@ -1,9 +1,13 @@
 package pl.nalazek.githubsearch;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * This class represents a query task that is executed when:
@@ -19,16 +23,28 @@ import okhttp3.Request;
  */
 public class QueryTask extends AsyncTask<Query, Void, ResponsePackage> {
 
+    final static String LOG_TAG = "QueryTask";
+
     //todo: add onCanecelled(Object) method (running on UI)
     @Override
     protected ResponsePackage doInBackground(Query... queries) {
 
         OkHttpClient client = new OkHttpClient();
+        ResponsePackage responsePackage = new ResponsePackage();
 
         // execute all queries
         for(Query query : queries) {
             Request request = new Request.Builder().url(query.getURL()).build();
+            try {
+                Response response = client.newCall(request).execute();
+                if(response.isSuccessful()) responsePackage.addResponse(response.body().string());
+                else responsePackage.addMessage(response.message());
+            } catch (IOException e) {
+                Log.e(LOG_TAG, e.getMessage());
+            }
         }
+
+
 
         //todo: check periodically isCancelled() value
         return null;

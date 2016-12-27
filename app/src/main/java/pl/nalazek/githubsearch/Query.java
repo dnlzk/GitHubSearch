5e8488194cd89d7bc2fe5912669c57d6e7256b1c
Daniel Nalazek <d.nlzk@o2.pl>
2016-12-27 15:08:49 +0100

    Added attrs to Query class

--- app/src/main/java/pl/nalazek/githubsearch/Query.java
+++ app/src/main/java/pl/nalazek/githubsearch/Query.java
@@ -1,9 +1,24 @@
 package pl.nalazek.githubsearch;
 
+import android.view.View;
+
+import java.net.URL;
+import java.util.LinkedList;
+
 /**
- * todo code edition
+ * This class is representing a Query.
  * @author Daniel Nalazek
  */
 public class Query {
+    final static String GITHUB_HOST_URL = "https://api.github.com";
+    final static String SEARCH_FOR_REPOS_URL = "/search/repositories";
+    final static String SEARCH_FOR_USERS_URL = "/search/users";
+    private URL url;
+    private String header;
+    private View view;
+    private String phrase;
+    private Boolean isSingle = true;
+    private Boolean isLast = true;
+    private LinkedList<Response> previousResponses;
 
 }

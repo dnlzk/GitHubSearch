f517ccd3028a4b44f42edbd64be123e111ad7cb2
Daniel Nalazek <d.nlzk@o2.pl>
2016-12-27 18:23:24 +0100

    added addResponse() method and responses ArrayList

--- app/src/main/java/pl/nalazek/githubsearch/ResponsePackage.java
+++ app/src/main/java/pl/nalazek/githubsearch/ResponsePackage.java
@@ -1,9 +1,22 @@
 package pl.nalazek.githubsearch;
 
+import java.util.ArrayList;
+
 /**
  * todo code edition
  * @author Daniel Nalazek
  */
 public class ResponsePackage
 {
+    private ArrayList<String> responses;
+
+    /**
+     * Adds a new response to the responses array
+     * @param response response string to add
+     * @return returns the size of the responses array
+     */
+    public int addResponse(String response) {
+        responses.add(response);
+        return responses.size();
+    }
 }

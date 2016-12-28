b0236231595cb121f83315f3a6035c5e5ec99306
Daniel Nalazek <d.nlzk@o2.pl>
2016-12-28 17:16:12 +0100

    added deserializing for user search in ResponsePartioned class

--- app/src/main/java/pl/nalazek/githubsearch/JsonObject.java
+++ app/src/main/java/pl/nalazek/githubsearch/JsonObject.java
@@ -1,6 +1,7 @@
 package pl.nalazek.githubsearch;
 
 /**
+ * This abstract class is used for inheriting by classes that keep deserialized data from a JSON stream
  * @author Daniel Nalazek
  */
 public abstract class JsonObject {
--- app/src/main/java/pl/nalazek/githubsearch/ResponsePartitioned.java
+++ app/src/main/java/pl/nalazek/githubsearch/ResponsePartitioned.java
@@ -1,7 +1,10 @@
 package pl.nalazek.githubsearch;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import okhttp3.Headers;
-import okhttp3.internal.http2.Header;
 
 /**
  * @author Daniel Nalazek
@@ -11,10 +14,46 @@ public class ResponsePartitioned {
     private String body;
     private ExchangeType exchangeType;
     private JsonObject jsonObject;
+    private Gson gson;
 
+    public ExchangeType getExchangeType() {
+        return exchangeType;
+    }
+
+    public Headers getHeaders() {
+        return headers;
+    }
+
+    public JsonObject getJsonObject() {
+        return jsonObject;
+    }
+
+    /**
+     * Constructor. Automatically converts HTML body to JsonObject in depending on ExchangeType
+     * @param headers Headers of the Response
+     * @param body Body of the Response
+     * @param exchangeType Type of exchange
+
+     */
     public ResponsePartitioned(Headers headers, String body, ExchangeType exchangeType) {
         this.body = body;
         this.headers = headers;
         this.exchangeType = exchangeType;
+        gson = createGson(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+
+    }
+
+    private void deserializeJSON() {
+        switch(exchangeType) {
+            case USER_SEARCH:
+                jsonObject = gson.fromJson(body, UserSearchResult.class);
+                break;
+        }
+    }
+
+    private Gson createGson(FieldNamingPolicy fieldNamingPolicy) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setFieldNamingPolicy(fieldNamingPolicy);
+        return gsonBuilder.create();
     }
 }

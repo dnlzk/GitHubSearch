818b08a869d999255f53c17cb3aba860f27107a4
Daniel Nalazek <d.nlzk@o2.pl>
2018-01-02 17:31:44 +0100

    Added UserDetailedActivity

---
+++ app/src/main/java/pl/nalazek/githubsearch/UserDetailedActivity.java
@@ -0,0 +1,21 @@
+package pl.nalazek.githubsearch;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class UserDetailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_detailed);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}

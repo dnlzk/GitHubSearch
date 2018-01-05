3ea7a3514f29f3477d650b6ebaecffaa3bab032e
Daniel Nalazek <d.nlzk@o2.pl>
2018-01-05 22:55:20 +0100

    changed: - filled in GitHubRepository Interface

--- app/src/main/java/pl/nalazek/githubsearch/data/GitHubRepository.java
+++ app/src/main/java/pl/nalazek/githubsearch/data/GitHubRepository.java
@@ -1,10 +1,30 @@
 package pl.nalazek.githubsearch.data;
 
+import java.util.ArrayList;
+
+import pl.nalazek.githubsearch.data.ResultObjects.Result;
+import pl.nalazek.githubsearch.data.ResultObjects.UserDetailedResult;
+import pl.nalazek.githubsearch.data.ResultObjects.UserSearchResult;
+
 /**
+ * This interface is the main entry point for accessing GitHub repository
  * @author Daniel Nalazek
  */
 
 public interface GitHubRepository {
 
+    void requestSearch(String phrase, GitHubRepositorySearchOptions searchOptions);
+
+    void requestUserDetailed(UserSearchResult userSearchResult);
+
+    interface SearchResultsCallback {
+
+        void onSearchResultsReady(ArrayList<Result> results);
+    }
+
+    interface UserDetailedCallback {
+
+        void onUserDetailedResultReady(UserDetailedResult result);
+    }
 
 }

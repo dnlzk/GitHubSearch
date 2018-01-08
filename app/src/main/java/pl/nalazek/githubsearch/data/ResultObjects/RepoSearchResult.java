f596a0decc33daf3b814d1773b2e9ee4e18642d0
Daniel Nalazek <d.nlzk@o2.pl>
2018-01-08 20:30:20 +0100

    implemented Parcelable for passing object in intents

--- app/src/main/java/pl/nalazek/githubsearch/data/ResultObjects/RepoSearchResult.java
+++ app/src/main/java/pl/nalazek/githubsearch/data/ResultObjects/RepoSearchResult.java
@@ -1,5 +1,7 @@
 package pl.nalazek.githubsearch.data.ResultObjects;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import pl.nalazek.githubsearch.data.ExchangeType;
 
 /**
@@ -8,7 +10,45 @@ import pl.nalazek.githubsearch.data.ExchangeType;
 
 public class RepoSearchResult extends SearchResult {
 
-    public RepoSearchResult(String title, String descrition, ExchangeType exchangeType) {
-        super(title, descrition, exchangeType);
+    private String repoURL;
+
+    public RepoSearchResult(String title, String description, String repoURL, ExchangeType exchangeType) {
+        super(title, description, exchangeType);
+        this.repoURL = repoURL;
+    }
+
+    public static final Parcelable.Creator<RepoSearchResult> CREATOR = new Parcelable.Creator<RepoSearchResult>() {
+        public RepoSearchResult createFromParcel(Parcel in) {
+            String[] parcelData = new String[3];
+            in.readStringArray(parcelData);
+            return buildFromParcelData(parcelData);
+        }
+
+        public RepoSearchResult[] newArray(int size) {
+            return new RepoSearchResult[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringArray(new String[] {
+                getTitle(),
+                getDescription(),
+                this.repoURL,
+        });
+    }
+
+    private static RepoSearchResult buildFromParcelData(String[] parcelData) {
+        return new RepoSearchResult(
+                parcelData[0],
+                parcelData[1],
+                parcelData[2],
+                ExchangeType.REPOS_SEARCH
+        );
     }
 }

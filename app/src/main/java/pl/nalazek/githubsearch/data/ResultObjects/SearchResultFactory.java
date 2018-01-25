7ebd6b5f8dd2a0d206fe3713d2ff00ab04e6658a
Daniel Nalazek <d.nlzk@o2.pl>
2018-01-25 20:23:51 +0100

    Added unit test coverage

---
+++ app/src/main/java/pl/nalazek/githubsearch/data/ResultObjects/SearchResultFactory.java
@@ -0,0 +1,89 @@
+package pl.nalazek.githubsearch.data.ResultObjects;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import pl.nalazek.githubsearch.data.JsonObjects.*;
+import pl.nalazek.githubsearch.data.ResponsePackage;
+import pl.nalazek.githubsearch.data.ResponsePartitioned;
+
+/**
+ * @author Daniel Nalazek
+ */
+
+public class SearchResultFactory implements ResultFactory {
+
+    private static final SearchResultFactory instance = new SearchResultFactory();
+
+    private SearchResultFactory() {}
+
+    public static SearchResultFactory getInstance() {
+        return instance;
+    }
+
+    @Override
+    public SearchResult[] makeResults(ResponsePartitioned responsePartitioned) throws InvalidJsonObjectException {
+
+        JsonObject jsonObject = responsePartitioned.getJsonObject();
+        if(jsonObject instanceof Itemable) return createResultsArray((Itemable)jsonObject);
+        else if(jsonObject == null) throw new InvalidJsonObjectException("Expected Itemable JsonObject! Got: null");
+        else throw new InvalidJsonObjectException("Expected Itemable JsonObject! Got: " + jsonObject.getClass().getName() + ". Try another using another ResultFactory");
+    }
+
+    @Override
+    public List<SearchResult> makeResults(ResponsePackage responsePackage) throws InvalidJsonObjectException {
+
+        ArrayList<ResponsePartitioned> responsesPartitioned = responsePackage.getResponses();
+        List<SearchResult> results = new ArrayList<>();
+
+        for(ResponsePartitioned responsePartitioned : responsesPartitioned) {
+            SearchResult[] searchResults = makeResults(responsePartitioned);
+            List<SearchResult> loopResults = Arrays.asList(searchResults);
+            results.addAll(loopResults);
+        }
+
+        return results;
+    }
+
+    private SearchResult[] createResultsArray(Itemable itemable) throws InvalidJsonObjectException {
+
+        List<? extends JsonItem> jsonItems = itemable.getItems();
+        return iterateOverItemsAndCreateResults(jsonItems);
+    }
+
+    private SearchResult[] iterateOverItemsAndCreateResults(List<? extends JsonItem> jsonItems) throws InvalidJsonObjectException {
+
+        int size = jsonItems.size();
+        SearchResult[] results = new SearchResult[size];
+        int i = 0;
+
+        for(JsonItem item : jsonItems) {
+            results[i++] = createResult(item);
+        }
+
+        return results;
+    }
+
+    private SearchResult createResult(JsonItem item) throws InvalidJsonObjectException {
+
+            if (item instanceof JsonRepoSearchResult.Item) {
+                return createRepoSearchResult((JsonRepoSearchResult.Item) item);
+            }
+            else if (item instanceof JsonUserSearchResult.Item) {
+                return createUserSearchResult((JsonUserSearchResult.Item) item);
+            }
+            else throw new InvalidJsonObjectException("JsonItem object not recognized! Got: " + item.getClass().getName());
+    }
+
+    private RepoSearchResult createRepoSearchResult(JsonRepoSearchResult.Item item) {
+        return new RepoSearchResult(item.getName(), item.getHtmlUrl(), item.getUrl());
+    }
+
+    private UserSearchResult createUserSearchResult(JsonUserSearchResult.Item item) {
+        return new UserSearchResult(item.getLogin(), item.getHtmlUrl(), item.getUrl(), item.getStarredUrl(), item.getAvatarUrl());
+    }
+
+
+}
+
---
+++ app/src/test/java/pl/nalazek/githubsearch/data/ResultObjects/SearchResultFactoryTest.java
@@ -0,0 +1,138 @@
+package pl.nalazek.githubsearch.data.ResultObjects;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import okhttp3.Headers;
+import okhttp3.ResponseBody;
+import pl.nalazek.githubsearch.data.ExchangeType;
+import pl.nalazek.githubsearch.data.JsonObjects.JsonUserDetailed;
+import pl.nalazek.githubsearch.data.QueryObjects.SearchQuery;
+import pl.nalazek.githubsearch.data.ResponsePackage;
+import pl.nalazek.githubsearch.data.ResponsePartitioned;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Daniel Nalazek
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SearchResultFactoryTest {
+
+    @Mock
+    ResponseBody body;
+    @Mock
+    Headers headers;
+
+    String title = "dzharii";
+    String url = "https://github.com/dzharii";
+    SearchResultFactory searchResultFactory;
+
+    @Before
+    public void before() throws Exception {
+        searchResultFactory = SearchResultFactory.getInstance();
+
+        when(body.string()).thenReturn("{\n" +
+                "  \"total_count\": 1,\n" +
+                "  \"incomplete_results\": false,\n" +
+                "  \"items\": [\n" +
+                "    {\n" +
+                "      \"login\": "+ title +",\n" +
+                "      \"id\": 36020,\n" +
+                "      \"avatar_url\": \"https://avatars3.githubusercontent.com/u/36020?v=4\",\n" +
+                "      \"gravatar_id\": \"\",\n" +
+                "      \"url\": \"https://api.github.com/users/dzharii\",\n" +
+                "      \"html_url\": \""+ url +"\",\n" +
+                "      \"followers_url\": \"https://api.github.com/users/dzharii/followers\",\n" +
+                "      \"following_url\": \"https://api.github.com/users/dzharii/following{/other_user}\",\n" +
+                "      \"gists_url\": \"https://api.github.com/users/dzharii/gists{/gist_id}\",\n" +
+                "      \"starred_url\": \"https://api.github.com/users/dzharii/starred{/owner}{/repo}\",\n" +
+                "      \"subscriptions_url\": \"https://api.github.com/users/dzharii/subscriptions\",\n" +
+                "      \"organizations_url\": \"https://api.github.com/users/dzharii/orgs\",\n" +
+                "      \"repos_url\": \"https://api.github.com/users/dzharii/repos\",\n" +
+                "      \"events_url\": \"https://api.github.com/users/dzharii/events{/privacy}\",\n" +
+                "      \"received_events_url\": \"https://api.github.com/users/dzharii/received_events\",\n" +
+                "      \"type\": \"User\",\n" +
+                "      \"site_admin\": false,\n" +
+                "      \"score\": 52.629246\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}\n");
+    }
+
+    @Test
+    public void whenGetInstanceThenSameReference() throws Exception {
+        SearchResultFactory searchResultFactoryForCompare = SearchResultFactory.getInstance();
+        assertThat("Static reference fault", searchResultFactory, is(searchResultFactoryForCompare));
+    }
+
+    @Test
+    public void givenResponsePackageWithoutResponsesWhenMakeResultsThenEmptyList() throws Exception {
+        ResponsePackage responsePackage = new ResponsePackage(SearchQuery.TYPE, "");
+        List<SearchResult> list = searchResultFactory.makeResults(responsePackage);
+        assertTrue("Results empty list fault", list.isEmpty());
+    }
+
+    @Test
+    public void givenResponsePartitionedUserSearchWhenMakeResultsThenArrayLenghtOne() throws Exception {
+        ResponsePartitioned responsePartitioned = new ResponsePartitioned(headers, body, ExchangeType.USER_SEARCH);
+        SearchResult[] searchResults = searchResultFactory.makeResults(responsePartitioned);
+        assertThat("Results array lenght fault", searchResults.length, is(1));
+    }
+
+    @Test
+    public void givenResponsePartitionedRepoSearchWhenMakeResultsThenArrayLenghtOne() throws Exception {
+        ResponsePartitioned responsePartitioned = new ResponsePartitioned(headers, body, ExchangeType.REPOS_SEARCH);
+        SearchResult[] searchResults = searchResultFactory.makeResults(responsePartitioned);
+        assertThat("Results array lenght fault", searchResults.length, is(1));
+    }
+
+    @Test
+    public void givenResponsePartitionedWhenMakeResultsAndGetTitleThenTitle() throws Exception {
+        ResponsePartitioned responsePartitioned = new ResponsePartitioned(headers, body, ExchangeType.USER_SEARCH);
+        SearchResult[] searchResults = searchResultFactory.makeResults(responsePartitioned);
+        assertThat("Results title fault", searchResults[0].getTitle(), is(title));
+    }
+
+    @Test
+    public void givenResponsePartitionedWhenMakeResultsAndGetDescriptionThenUrl() throws Exception {
+        ResponsePartitioned responsePartitioned = new ResponsePartitioned(headers, body, ExchangeType.USER_SEARCH);
+        SearchResult[] searchResults = searchResultFactory.makeResults(responsePartitioned);
+        assertThat("Results description fault", searchResults[0].getDescription(), is(url));
+    }
+
+    @Test(expected = InvalidJsonObjectException.class)
+    public void givenResponsePartitionedWhenMakeResultsThenInvalidJSONObjectException() throws Exception {
+        ResponsePartitioned responsePartitioned = mock(ResponsePartitioned.class);
+        when(responsePartitioned.getJsonObject()).thenReturn(new JsonUserDetailed());
+        searchResultFactory.makeResults(responsePartitioned);
+    }
+
+    @Test
+    public void givenResponsePackageWithMessagesWhenMakeResultsThenSizeIsOne() throws Exception {
+        ResponsePackage responsePackage = createResponsePackageMock();
+        List<SearchResult> list = searchResultFactory.makeResults(responsePackage);
+        assertThat("List size fault", list.size(), is(1));
+    }
+
+    private ResponsePackage createResponsePackageMock() {
+        ResponsePackage responsePackage = mock(ResponsePackage.class);
+        ArrayList<ResponsePartitioned> list = new ArrayList<> (
+                Collections.singletonList(
+                        new ResponsePartitioned(headers, body, ExchangeType.USER_SEARCH)
+                ));
+        when(responsePackage.getResponses()).thenReturn(list);
+
+        return responsePackage;
+    }
+}

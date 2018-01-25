c33cd706be1acdf364ad7671412df8dc8386afba
Daniel Nalazek <d.nlzk@o2.pl>
2018-01-25 14:59:55 +0100

    Changed: - added unit test coverage

--- app/src/main/java/pl/nalazek/githubsearch/data/ResultObjects/UserDetailedResult.java
+++ app/src/main/java/pl/nalazek/githubsearch/data/ResultObjects/UserDetailedResult.java
@@ -5,18 +5,39 @@ import pl.nalazek.githubsearch.data.ExchangeType;
 /**
  * @author Daniel Nalazek
  */
+public class UserDetailedResult extends DetailedResult {
 
-public class UserDetailedResult extends Result {
-
-
+    public final static String TYPE = "UserDetailedResult";
 
     private String userName;
     private int followers;
     private int stars;
     private byte[] avatarImage;
 
+
+    /**
+     * @param userName Name of user
+     * @param followers Count of followers
+     * @param stars Count of stars
+     * @param avatarImage Avatar image
+     * @param exchangeType Type of Exchange
+     */
     public UserDetailedResult(String userName, int followers, int stars, byte[] avatarImage, ExchangeType exchangeType) {
         super(exchangeType);
+        this.userName = userName;
+        this.followers = followers;
+        this.stars = stars;
+        this.avatarImage = avatarImage;
+    }
+
+    /**
+     * @param userName Name of user
+     * @param followers Count of followers
+     * @param stars Count of stars
+     * @param avatarImage Avatar image
+     */
+    public UserDetailedResult(String userName, int followers, int stars, byte[] avatarImage) {
+        super(ExchangeType.USER_DETAILED);
         this.userName = userName;
         this.followers = followers;
         this.stars = stars;
@@ -37,5 +58,10 @@ public class UserDetailedResult extends Result {
 
     public byte[] getAvatarImage() {
         return avatarImage;
+    }
+
+    @Override
+    public String getResultType() {
+        return TYPE;
     }
 }

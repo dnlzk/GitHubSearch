9a29a8c60f2e0e8309e078578dfa8501b9abdbfe
Daniel Nalazek <d.nlzk@o2.pl>
2018-01-31 19:22:52 +0100

    Added ResultCreator for safe Result objects construction Added unit tests

---
+++ app/src/main/java/pl/nalazek/githubsearch/data/ResultObjects/ResultCreator.java
@@ -0,0 +1,100 @@
+package pl.nalazek.githubsearch.data.ResultObjects;
+
+import java.io.InvalidObjectException;
+import java.util.Collections;
+import java.util.List;
+
+import pl.nalazek.githubsearch.data.ExchangeType;
+import pl.nalazek.githubsearch.data.QueryObjects.SearchQuery;
+import pl.nalazek.githubsearch.data.QueryObjects.UserDetailedQuery;
+import pl.nalazek.githubsearch.data.ResponsePackage;
+import pl.nalazek.githubsearch.data.ResponsePartitioned;
+
+/**
+ * A creator which makes results. Provides creating-safe {@link SearchResult} and {@link DetailedResult} concrete objects.
+ * @author Daniel Nalazek
+ */
+public class ResultCreator implements ResultFactory {
+
+    private ResultFactory resultFactory;
+
+
+    /**
+     * Creates results depending on passed {@link ResponsePartitioned}.
+     * @return {@link SearchResult} or {@link DetailedResult} arrays. If passed a response with an unrecognized exchange type,
+     * an empty array will be returned.
+     */
+    @Override
+    public Result[] makeResults(ResponsePartitioned responsePartitioned) {
+
+        ExchangeType type = responsePartitioned.getExchangeType();
+
+        switch(type) {
+            case USER_SEARCH:
+            case REPOS_SEARCH:
+                resultFactory = SearchResultFactory.getInstance();
+                return makeResultsArray(responsePartitioned);
+
+            case USER_DETAILED:
+            case USER_DETAILED_STARS:
+            case USER_DETAILED_AVATAR:
+                resultFactory = new DetailedResultFactory();
+                return makeResultsArray(responsePartitioned);
+
+            default:
+                return new Result[0];
+        }
+    }
+
+
+
+    /**
+     * Creates results list depending on passed {@link ResponsePackage}.
+     * @return {@link SearchResult} or {@link DetailedResult} lists. If passed a response package
+     * with an unrecognized {@link pl.nalazek.githubsearch.data.QueryObjects.Query} type an empty list will be returned.
+     */
+    @Override
+    public List<? extends Result> makeResults(ResponsePackage responsePackage) {
+
+        String queryType = responsePackage.getQueryType();
+
+        switch(queryType) {
+            case SearchQuery.TYPE:
+                resultFactory = SearchResultFactory.getInstance();
+                return makeResultsList(responsePackage);
+
+            case UserDetailedQuery.TYPE:
+                resultFactory = new DetailedResultFactory();
+                return makeResultsList(responsePackage);
+
+            default:
+                return Collections.emptyList();
+        }
+
+    }
+
+
+
+    private Result[] makeResultsArray(ResponsePartitioned responsePartitioned) {
+
+        try {
+            return resultFactory.makeResults(responsePartitioned);
+        }
+        catch (InvalidJsonObjectException e) {
+            e.printStackTrace();
+            return new Result[0];
+        }
+    }
+
+
+    private List<? extends Result> makeResultsList(ResponsePackage responsePackage) {
+
+        try {
+            return resultFactory.makeResults(responsePackage);
+        }
+        catch (InvalidJsonObjectException | InvalidObjectException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+}
---
+++ app/src/test/java/pl/nalazek/githubsearch/data/ResultObjects/ResultCreatorTest.java
@@ -0,0 +1,162 @@
+package pl.nalazek.githubsearch.data.ResultObjects;
+
+import com.google.common.base.Charsets;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import okhttp3.Headers;
+import okhttp3.ResponseBody;
+import pl.nalazek.githubsearch.data.*;
+import pl.nalazek.githubsearch.data.QueryObjects.*;
+import pl.nalazek.githubsearch.util.TextToString;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Daniel Nalazek
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ResultCreatorTest {
+
+    private final static String JSON_FILE_SEARCH = "testInputs/searchResult.json";
+    private final static String JSON_USER_DETAILED = "testInputs/userDetailedSingleton.json";
+    private final static String JSON_FILE_STARRED_PAGE_LAST_LINKHEADER = "testInputs/starLastPage.linkhead";
+    private final static String JSON_FILE_STARRED_PAGE_LAST = "testInputs/starLastPage.json";
+
+    @Mock
+    ResponsePartitioned responsePartitioned;
+    @Mock
+    ResponsePackage responsePackage;
+    @Mock
+    ResponseBody body;
+    @Mock
+    ResponseBody bodyUserDetailed;
+    @Mock
+    Headers headers;
+    @Mock
+    ResponseBody bodyStarsLastPage;
+    @Mock
+    Headers headersStarsLastPage;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+
+    ResultCreator creator = new ResultCreator();
+
+
+    @Before
+    public void before() throws Exception {
+        setupMocks();
+    }
+
+
+
+    @Test
+    public void givenResponsePartionedExchangeTypePageWhenMakeResultsThenArrayLenghtZero() throws Exception {
+
+        when(responsePartitioned.getExchangeType()).thenReturn(ExchangeType.USER_PAGE);
+
+        Result[] results = creator.makeResults(responsePartitioned);
+        assertThat("Result size zero fault", results.length, is(0));
+    }
+
+
+    @Test
+    public void givenResponsePartionedExchangeUserSearchWhenMakeResultsThenArrayLenghtEight() throws Exception {
+
+        ResponsePartitioned response = new ResponsePartitioned(headers, body, QueryTask.STATE_SUCCESS, ExchangeType.USER_SEARCH);
+
+        Result[] results = creator.makeResults(response);
+        assertThat("Result size fault", results.length, is(8));
+    }
+
+
+
+    @Test
+    public void givenResponsePartionedExchangeUserDetailedWhenMakeResultsThenArrayLenghtOne() throws Exception {
+
+        ResponsePartitioned response = new ResponsePartitioned(headers, bodyUserDetailed, QueryTask.STATE_SUCCESS, ExchangeType.USER_DETAILED);
+
+        Result[] results = creator.makeResults(response);
+        assertThat("Result size fault", results.length, is(1));
+    }
+
+
+
+    @Test
+    public void givenResponsePartionedExchangeSearchWhenMakeResultsThenArrayLenghtZero() throws Exception {
+        when(responsePartitioned.getExchangeType()).thenReturn(ExchangeType.USER_SEARCH);
+        when(responsePartitioned.getJsonObject()).thenReturn(null);
+
+        Result[] results = creator.makeResults(responsePartitioned);
+        assertThat("Result size fault", results.length, is(0));
+    }
+
+
+    @Test
+    public void givenResponsePackageTypeNewWhenMakeResultsThenListEmpty() throws Exception {
+
+        when(responsePackage.getQueryType()).thenReturn("New type");
+
+        List<? extends Result> results = creator.makeResults(responsePackage);
+        assertTrue("Result empty list fault", results.isEmpty());
+    }
+
+
+    @Test
+    public void givenEmptyResponsePackageSearchQueryTypeWhenMakeResultsThenListEmpty() throws Exception {
+
+        responsePackage = new ResponsePackage(SearchQuery.TYPE);
+
+        List<? extends Result> results = creator.makeResults(responsePackage);
+        assertTrue("Result empty list fault", results.isEmpty());
+    }
+
+
+    @Test
+    public void givenEmptyResponsePackageUserDetailedQueryTypeWhenMakeResultsThenListEmpty() throws Exception {
+
+        responsePackage = new ResponsePackage(UserDetailedQuery.TYPE);
+
+        List<? extends Result> results = creator.makeResults(responsePackage);
+        assertTrue("Result empty list fault", results.isEmpty());
+    }
+
+
+    @Test
+    public void givenInvalidResponsePackageWhenMakeResultsThenEmptyList() throws Exception {
+
+        responsePackage = new ResponsePackage(UserDetailedQuery.TYPE);
+        responsePackage.addResponses(Collections.singletonList(
+                new ResponsePartitioned(headersStarsLastPage, bodyStarsLastPage, QueryTask.STATE_SUCCESS, ExchangeType.USER_DETAILED_STARS)
+                )
+        );
+
+        List<? extends Result> results = creator.makeResults(responsePackage);
+        assertTrue("Result empty list with exception fault", results.isEmpty());
+    }
+
+
+
+    private void setupMocks() throws Exception {
+
+        when(body.string()).thenReturn(TextToString.read(JSON_FILE_SEARCH, Charsets.UTF_16));
+        when(bodyUserDetailed.string()).thenReturn(TextToString.read(JSON_USER_DETAILED, Charsets.UTF_16));
+        when(headers.get("Link")).thenReturn(null);
+        when(headersStarsLastPage.get("Link")).thenReturn(TextToString.read(JSON_FILE_STARRED_PAGE_LAST_LINKHEADER, Charsets.UTF_16));
+        when(bodyStarsLastPage.string()).thenReturn(TextToString.read(JSON_FILE_STARRED_PAGE_LAST, Charsets.UTF_16));
+    }
+}

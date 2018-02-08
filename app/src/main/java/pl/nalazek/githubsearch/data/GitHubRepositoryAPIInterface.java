bdd773644a97effe4b7eab4088064c76038de444
Daniel Nalazek <d.nlzk@o2.pl>
2018-02-08 18:56:01 +0100

    created interface methods

--- app/src/main/java/pl/nalazek/githubsearch/data/GitHubRepositoryAPIInterface.java
+++ app/src/main/java/pl/nalazek/githubsearch/data/GitHubRepositoryAPIInterface.java
@@ -1,10 +1,54 @@
 package pl.nalazek.githubsearch.data;
 
+import android.support.annotation.NonNull;
+
+import pl.nalazek.githubsearch.data.QueryObjects.QueryTask;
+import pl.nalazek.githubsearch.data.ResultObjects.SearchResult;
+
 /**
  * @author Daniel Nalazek
  */
 
 public interface GitHubRepositoryAPIInterface {
 
+    void startSearch(@NonNull String phrase,
+                     @NonNull GitHubRepositorySearchOptions searchOptions,
+                     @NonNull SearchAPICallback res);
 
+
+
+    void getDetailedData(@NonNull SearchResult searchResult,
+                         @NonNull DetailedDataAPICallback detailedDataCallback);
+
+
+
+    /*
+     * Finishes all connections with external data storages. Should be always called to free resources.
+     */
+    void finish() throws InterruptedException;
+
+
+
+    /**
+     * Stops last task. Usually used to avoid no more needed result callbacks.
+     */
+    void stopLastTask();
+
+
+
+    interface ResultsAPICallback {
+        void onResultsReady(ResponsePackage responsePackage);
+        void onError(String message);
+    }
+
+
+    interface SearchAPICallback extends ResultsAPICallback {}
+
+
+    interface DetailedDataAPICallback extends ResultsAPICallback {}
+
+
+    interface QueryTaskCallback {
+        void onResponseReady(QueryTask queryTask, ResponsePackage responsePackage);
+    }
 }

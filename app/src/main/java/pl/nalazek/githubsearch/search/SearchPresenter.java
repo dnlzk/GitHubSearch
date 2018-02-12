c67733285ca8ee32b7e8587d2708ac8020b90b87
Daniel Nalazek <d.nlzk@o2.pl>
2018-02-12 14:36:02 +0100

    Added unit test coverage Added support for invalid per page arguments

--- app/src/main/java/pl/nalazek/githubsearch/search/SearchPresenter.java
+++ app/src/main/java/pl/nalazek/githubsearch/search/SearchPresenter.java
@@ -1,64 +1,65 @@
 package pl.nalazek.githubsearch.search;
 
-import java.util.HashSet;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.List;
 
-import pl.nalazek.githubsearch.Showable;
+import pl.nalazek.githubsearch.data.GitHubRepository;
 import pl.nalazek.githubsearch.data.GitHubRepositorySearchOptions;
-import pl.nalazek.githubsearch.data.QueryObjects.QueryHistory;
-import pl.nalazek.githubsearch.data.QueryObjects.Query;
-import pl.nalazek.githubsearch.data.QueryObjects.QueryBuilder;
 import pl.nalazek.githubsearch.data.QueryObjects.SearchQuery;
-import pl.nalazek.githubsearch.data.QueryObjects.QueryTask;
-import pl.nalazek.githubsearch.data.ResponsePackage;
-import pl.nalazek.githubsearch.data.ResultObjects.ResultArrayListBuilder;
+import pl.nalazek.githubsearch.data.ResultObjects.Result;
 
 /**
- * This singleton class is responsible for the whole search process and displaying results on a SearchResultListAdapter
+ * This class is a controller responsible for piping requests and callbacks between the view and the model.
  * @author Daniel Nalazek
  */
-public class SearchPresenter implements Observer {
-
-    public enum SearchScope { USERS, REPOSITORIES }
-
-    static final String CHECK_IN_HISTORY = "CheckInHistory";
-
-    private static SearchPresenter instance = new SearchPresenter();
-    private static final String LOG_TAG = "SearchPresenter";
-    private int pResultsPerPage = 50;
-    private Boolean pScopeUsers = true;
-    private Boolean pScopeRepos = true;
-    private SearchQuery.Sort pSorting = null;
-    private SearchQuery.Order pOrdering = null;
-    private ResponsePackage actualResponsePackageOnView = null;
-    private QueryTask actualProcessingTask = null;
-    private HashSet<Integer> pageCache;
-    private static QueryHistory queryHistory;
+public class SearchPresenter implements SearchContract.UserActionListener {
 
 
-    /**
-     * Gets an instance of a single SearchPresenter class
-     * @return Single instance of a SearchPresenter class
-     */
-    public static SearchPresenter getInstance() {
-        return instance;
+    public static final String CHECK_IN_HISTORY = "CheckInHistory";
+    public static final String STOP_SEARCH = "StopSearch";
+    public static final String RESULTS_PER_PAGE_OUT_OF_RANGE_ERROR = "Results per page setting " +
+            "out of range! Acceptable values are between 1 and 100.";
+    public static final short DEFAULT_PER_PAGE_VALUE = 50;
+
+    private short resultsPerPage = DEFAULT_PER_PAGE_VALUE;
+    private Boolean scopeUsers = true;
+    private Boolean scopeRepos = true;
+    private SearchQuery.Sort sorting = SearchQuery.Sort.BEST;
+    private SearchQuery.Order ordering = SearchQuery.Order.ASCENDING;
+    private boolean searchInHistory = false;
+    private GitHubRepositorySearchOptions options;
+
+    private SearchContract.Showable showable;
+    private GitHubRepository gitHubRepository;
+
+
+
+
+
+    public SearchPresenter(GitHubRepository gitHubRepository, SearchContract.Showable showable) {
+        this.gitHubRepository = gitHubRepository;
+        this.showable = showable;
+        setSearchOptions();
     }
+
+
 
     /**
      * Use to set the scope of searching
      * @param searchScope pass the SearchScope enums to define the scope or none to disable searching
      */
-    public void setSearchScope(SearchScope... searchScope) {
-        pScopeUsers = false;
-        pScopeRepos = false;
-        for(SearchScope scope : searchScope) {
+    public void setSearchScope(SearchQuery.SearchScope... searchScope) {
+        scopeUsers = false;
+        scopeRepos = false;
+        for(SearchQuery.SearchScope scope : searchScope) {
             switch(scope){
-                case USERS: pScopeUsers = true; break;
-                case REPOSITORIES: pScopeRepos = true; break;
+                case USERS: scopeUsers = true; break;
+                case REPOSITORIES: scopeRepos = true; break;
             }
         }
+        setSearchOptions();
     }
+
+
 
     /**
      * Use to set the number of results viewed. Note that if you set the scope both to USERS
@@ -66,105 +67,88 @@ public class SearchPresenter implements Observer {
      * Example: passing number = 30
      * if the scope is set only to USERS there will be 30 results on page,
      * if the scope is set to both USERS and REPOSITORIES there will be 60 results on page
-     * @param number the number of results
-     * @see SearchPresenter#setSearchScope(SearchScope...)
+     * @param number the number of results, MIN=1, MAX=100
+     * @see SearchPresenter#setSearchScope(SearchQuery.SearchScope...)
      */
-    public void setResultsNumber(int number){
-        pResultsPerPage = number;
+    public void setResultsNumber(short number){
+        resultsPerPage = number;
+        setSearchOptions();
     }
 
-    /**
-     * Use to get the the number of results shown
-     * @return number of results
-     */
-    public int getResultsNumber() {
-        return pResultsPerPage;
+
+
+    public GitHubRepositorySearchOptions getSearchOptions() {
+        return options;
     }
 
-    /**
-     * Use to start a search.
-     * This method creates a QueryTask, if an earlier QueryTask is still being processed then it is canceled.
-     * @param phrase The string to search for
-     * @param checkInHistory FALSE for beginning a new search, TRUE for checking results in search history and if found present them
-     * @param showable Showable object to show results on
-     */
-    public void searchForPhrase(String phrase, boolean checkInHistory, Showable showable) {
-
-        // Check if an other task is pending and cancel it
-        if(actualProcessingTask != null) actualProcessingTask.cancel(true);
-        showable.showBusy();
 
 
-
-        if(checkInHistory && getQueryHistory().isPhraseInHistory(phrase)) {
-            publishResultsFromHistory(phrase, showable);
-            return;
-        }
-
-        // Create a query array
-        QueryBuilder queryBuilder = new QueryBuilder();
-        Query[] queries = queryBuilder.setOrdering(pOrdering)
-                .setSorting(pSorting)
-                .setResultsPerPage(pResultsPerPage)
-                .setScope(pScopeUsers, pScopeRepos)
-                .build(phrase, showable);
-
-        // Create and set as actual new QueryTask
-        QueryTask queryTask = new QueryTask();
-        actualProcessingTask = queryTask;
-
-        //Execute a query task
-        queryTask.execute(queries);
-    }
-
-    /**
-     * Use to start a search for suggestions
-     * This method creates a QueryTask, if an earlier QueryTask is still being processed then it is canceled.
-     * @param suggestion The suggestion to search for
-     */
-    public void searchForSuggestion(String suggestion) {
-
-        // Check if an other task is pending and cancel it
-        if(actualProcessingTask != null) actualProcessingTask.cancel(true);
-        //todo
-    }
-
-    /**
-     * Called when a new pair is put into QueryHistory
-     * @param observable instance of QueryHistory
-     * @param o QueryTask instance which accessed the Observable object, here QueryHistory
-     */
     @Override
-    public void update(Observable observable, Object o) {
-        QueryHistory qHistory = (QueryHistory) observable;
-        QueryTask qTask = (QueryTask) o;
-        ResponsePackage rp = qHistory.get(qTask);
-
-        // task ended, set value to null
-        if(actualProcessingTask == qTask) actualProcessingTask = null;
-        actualResponsePackageOnView = rp;
+    public void finish() {
+        gitHubRepository.close();
     }
 
-    /**
-     * In most cases used to access Observable QueryHistory object by QueryTask when it finishes it task
-     * @return QueryHistory
-     */
-    public static QueryHistory getQueryHistory() {
-        return queryHistory;
+
+
+    @Override
+    public void requestSearch(String phrase, boolean searchInHistory) {
+
+        this.searchInHistory = searchInHistory;
+        showable.showBusy();
+        createSearchRequest(phrase, options);
     }
 
-    private void publishResultsFromHistory(String phrase,Showable showable) {
-        ResponsePackage responsePackage = getQueryHistory().get(phrase);
-        showable.showResults(ResultArrayListBuilder.build(responsePackage));
-        actualResponsePackageOnView = responsePackage;
+
+
+    @Override
+    public void stopSearch() {
+        gitHubRepository.stopSearch();
+        showable.hideBusy();
     }
 
-    /**
-     * Private constructor
-     */
-    private SearchPresenter() {
-        queryHistory = new QueryHistory();
-        queryHistory.addObserver(this);
+
+
+
+    private void createSearchRequest(String phrase, GitHubRepositorySearchOptions options) {
+
+        gitHubRepository.requestSearch(phrase, options,
+                new GitHubRepository.SearchResultsCallback() {
+
+                    @Override
+                    public void onSearchResultsReady(List<? extends Result> results) {
+                        showable.showResults(results);
+                    }
+
+                    @Override
+                    public void onError(String message) {
+                        showable.showError(message);
+                    }
+                });
+    }
+
+
+
+    private void setSearchOptions() {
+        options = tryToGetGitHubRepositorySearchOptions();
+    }
+
+
+
+    private GitHubRepositorySearchOptions tryToGetGitHubRepositorySearchOptions() {
+        try {
+                return   GitHubRepositorySearchOptions.build()
+                        .setOrdering(ordering)
+                        .setSorting(sorting)
+                        .setResultsPerPage(resultsPerPage)
+                        .setScope(scopeUsers, scopeRepos)
+                        .forceSearchInHistory(searchInHistory);
+        }
+        catch (IllegalArgumentException e) {
+
+            showable.showError(RESULTS_PER_PAGE_OUT_OF_RANGE_ERROR);
+            resultsPerPage = DEFAULT_PER_PAGE_VALUE;
+            return tryToGetGitHubRepositorySearchOptions();
+        }
     }
 }
 
---
+++ app/src/testMock/java/pl/nalazek/githubsearch/search/SearchPresenterTest.java
@@ -0,0 +1,201 @@
+package pl.nalazek.githubsearch.search;
+
+import android.support.annotation.NonNull;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import pl.nalazek.githubsearch.FakeGitHubRepositoryAPI;
+import pl.nalazek.githubsearch.Injection;
+import pl.nalazek.githubsearch.data.GitHubRepository;
+import pl.nalazek.githubsearch.data.GitHubRepositorySearchOptions;
+import pl.nalazek.githubsearch.data.QueryObjects.SearchQuery;
+import pl.nalazek.githubsearch.data.ResultObjects.Result;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.times;
+
+import static pl.nalazek.githubsearch.search.SearchPresenter.DEFAULT_PER_PAGE_VALUE;
+
+/**
+ * @author Daniel Nalazek
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SearchPresenterTest {
+
+
+    private SearchPresenter searchPresenter;
+    private static final String SEARCH_PHRASE = "test";
+
+    @Spy
+    SearchContract.Showable showable;
+    @Spy
+    GitHubRepository repo =  Mockito.spy(Injection.provideGitHubRepository());
+
+    @Captor
+    ArgumentCaptor<String> errorCaptor;
+    @Captor
+    ArgumentCaptor<List<? extends Result>> resultsCaptor;
+
+
+    @Before
+    public void before() {
+        searchPresenter = new SearchPresenter(repo,showable);
+    }
+
+
+
+    @Test
+    public void whenGetSearchOptionsThenDefaults() throws Exception {
+        GitHubRepositorySearchOptions options = searchPresenter.getSearchOptions();
+
+        assertThat("Order fault", options.getOrder(), is(SearchQuery.Order.ASCENDING));
+        assertThat("Sort fault", options.getSorting(), is(SearchQuery.Sort.BEST));
+        assertThat("Per page fault", options.getResultsPerPage(), is(DEFAULT_PER_PAGE_VALUE));
+        assertThat("Scope users fault", options.isScopeUsers(), is(true));
+        assertThat("Scope repos fault", options.isScopeRepos(), is(true));
+        assertThat("History force fault", options.isForcedSearchInHistory(), is(false));
+    }
+
+
+
+    @Test
+    public void whenSetWrongPerPageValueThenShowableOnErrorAndDefault() throws Exception {
+
+        short[] wrongValues = {-5,0,102};
+        for(short value : wrongValues) {
+            searchPresenter.setResultsNumber(value);
+            assertThat("Default per page fault", searchPresenter.getSearchOptions().getResultsPerPage(),
+                    is(DEFAULT_PER_PAGE_VALUE));
+        }
+        Mockito.verify(showable, times(3)).showError(errorCaptor.capture());
+    }
+
+
+
+    @Test
+    public void whenSetGoodPerPageValueThenNotShowableOnErrorAndValue() throws Exception {
+
+        short[] goodValues = {1,5,99};
+        for(short value : goodValues) {
+            searchPresenter.setResultsNumber(value);
+            assertThat("Per page fault", searchPresenter.getSearchOptions().getResultsPerPage(),
+                    is(value));
+        }
+        Mockito.verify(showable, times(0)).showError(errorCaptor.capture());
+    }
+
+
+
+    @Test
+    public void whenSetSearchScopeUsersFalseThenValue() throws Exception {
+
+        SearchQuery.SearchScope[][] scopes = getAllSearchScopesCombinations();
+
+        for(SearchQuery.SearchScope[] scope : scopes) {
+            searchPresenter.setSearchScope(scope);
+
+            if(scope.length == 0) {
+                assertAllFalseScope();
+            }
+            else {
+                    for (SearchQuery.SearchScope s : scope) {
+
+                        if (s.equals(SearchQuery.SearchScope.USERS))
+                            assertUsersScopeTrueOnly(scope);
+                        if (s.equals(SearchQuery.SearchScope.REPOSITORIES))
+                            assertReposScopeTrueOnly(scope);
+                    }
+            }
+        }
+    }
+
+
+
+    @Test
+    public void givenSameInstanceRepoWhenFinishThenCloseCallOnRepo() throws Exception {
+        searchPresenter.finish();
+        Mockito.verify(repo).close();
+    }
+
+
+
+    @Test
+    public void givenSameInstanceRepoWhenStopSearchThenStopSearchCallOnRepoAndHideBusy() throws Exception {
+        searchPresenter.stopSearch();
+        Mockito.verify(repo).stopSearch();
+        Mockito.verify(showable).hideBusy();
+    }
+
+
+
+    @Test
+    public void whenRequestedSearchWithPhraseThenShowBusyAndResultsCallback() throws Exception {
+        searchPresenter.requestSearch(SEARCH_PHRASE, true);
+        Mockito.verify(showable).showBusy();
+        Mockito.verify(showable, timeout(100)).showResults(resultsCaptor.capture());
+    }
+
+
+
+    @Test
+    public void whenRequestedSearchWithErrorPhraseThenShowBusyAndErrorCallback() throws Exception {
+        searchPresenter.requestSearch(FakeGitHubRepositoryAPI.GENERATE_ERROR_PHRASE, true);
+        Mockito.verify(showable).showBusy();
+        Mockito.verify(showable, timeout(100)).showError(errorCaptor.capture());
+    }
+
+
+
+
+    @NonNull
+    private SearchQuery.SearchScope[][] getAllSearchScopesCombinations() {
+
+        return new SearchQuery.SearchScope[][]{
+                {SearchQuery.SearchScope.USERS},
+                {SearchQuery.SearchScope.USERS, SearchQuery.SearchScope.REPOSITORIES},
+                {SearchQuery.SearchScope.REPOSITORIES},
+                {}
+        };
+    }
+
+
+
+    private void assertReposScopeTrueOnly(SearchQuery.SearchScope[] scope) {
+
+        assertThat("Repo scope true fault", searchPresenter.getSearchOptions()
+                .isScopeRepos(), is(true));
+        if(scope.length == 1) assertThat("User scope singleton fault", searchPresenter.getSearchOptions()
+                .isScopeUsers(), is(false));
+    }
+
+
+
+    private void assertUsersScopeTrueOnly(SearchQuery.SearchScope[] scope) {
+
+        assertThat("User scope true fault", searchPresenter.getSearchOptions()
+                .isScopeUsers(), is(true));
+        if(scope.length == 1) assertThat("Repo scope singleton fault", searchPresenter.getSearchOptions()
+                .isScopeRepos(), is(false));
+    }
+
+
+
+    private void assertAllFalseScope() {
+
+        assertThat("User scope false fault", searchPresenter.getSearchOptions().isScopeUsers(),
+                is(false));
+        assertThat("Repo scope false fault", searchPresenter.getSearchOptions().isScopeRepos(),
+                is(false));
+    }
+}

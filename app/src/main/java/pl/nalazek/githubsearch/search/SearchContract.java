262c549fedb18a8d8898faa17d928913fb6cb0a2
Daniel Nalazek <d.nlzk@o2.pl>
2018-02-14 16:54:12 +0100

    Added unit test coverage Added support for invalid per page arguments

--- app/src/main/java/pl/nalazek/githubsearch/search/SearchContract.java
+++ app/src/main/java/pl/nalazek/githubsearch/search/SearchContract.java
@@ -1,10 +1,37 @@
 package pl.nalazek.githubsearch.search;
 
+import java.util.List;
+
+import pl.nalazek.githubsearch.data.ResultObjects.Result;
+
 /**
  * @author Daniel Nalazek
  */
 
 public interface SearchContract {
+
+    interface Showable {
+
+        void showResults(List<? extends Result> resultsArray);
+
+        void showError(String error);
+
+        void showBusy();
+
+        void hideBusy();
+    }
+
+
+
+    interface UserActionListener {
+
+        void requestSearch(String phrase, boolean checkInHistory);
+
+        void finish();
+
+        void stopSearch();
+    }
+
 
 
 }

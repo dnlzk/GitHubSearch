89900ed719c5e1c6206cddfce97de88eef316580
Daniel Nalazek <d.nlzk@o2.pl>
2018-02-14 16:55:33 +0100

    added Interface for MVC

--- app/src/main/java/pl/nalazek/githubsearch/userdetails/UserDetailedContract.java
+++ app/src/main/java/pl/nalazek/githubsearch/userdetails/UserDetailedContract.java
@@ -1,8 +1,29 @@
 package pl.nalazek.githubsearch.userdetails;
 
+import java.util.List;
+
+import pl.nalazek.githubsearch.data.ResultObjects.Result;
+import pl.nalazek.githubsearch.data.ResultObjects.SearchResult;
+
 /**
  * @author Daniel Nalazek
  */
 
 public interface UserDetailedContract {
+
+    interface Showable {
+
+        void showResults(List<? extends Result> resultsArray);
+
+        void showError(String error);
+
+        void showBusy();
+    }
+
+
+
+    interface UserActionListener {
+
+        void requestDetailedData(SearchResult searchResult);
+    }
 }

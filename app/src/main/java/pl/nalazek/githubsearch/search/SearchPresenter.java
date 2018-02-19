784bd954ea4cd86e80d9c0f03bfe561146824090
Daniel Nalazek <d.nlzk@o2.pl>
2018-02-19 14:13:22 +0100

    added setting search options in constructor

--- app/src/main/java/pl/nalazek/githubsearch/search/SearchPresenter.java
+++ app/src/main/java/pl/nalazek/githubsearch/search/SearchPresenter.java
@@ -38,6 +38,7 @@ public class SearchPresenter implements SearchContract.UserActionListener {
     public SearchPresenter(GitHubRepository gitHubRepository, SearchContract.Showable showable) {
         this.gitHubRepository = gitHubRepository;
         this.showable = showable;
+        setSearchOptions();
     }
 
 

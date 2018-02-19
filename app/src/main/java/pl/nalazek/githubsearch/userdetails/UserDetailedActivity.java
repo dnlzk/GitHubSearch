da04bdd6784633e8a7bf30705a5335aab8f57a4e
Daniel Nalazek <d.nlzk@o2.pl>
2018-02-19 13:02:21 +0100

    added Showable interface and API callbacks support

--- app/src/main/java/pl/nalazek/githubsearch/userdetails/UserDetailedActivity.java
+++ app/src/main/java/pl/nalazek/githubsearch/userdetails/UserDetailedActivity.java
@@ -1,53 +1,105 @@
 package pl.nalazek.githubsearch.userdetails;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
+import pl.nalazek.githubsearch.Injection;
 import pl.nalazek.githubsearch.R;
-import pl.nalazek.githubsearch.data.QueryObjects.Query;
-import pl.nalazek.githubsearch.data.QueryObjects.QueryBuilder;
-import pl.nalazek.githubsearch.data.QueryObjects.QueryTask;
 import pl.nalazek.githubsearch.data.ResultObjects.Result;
+import pl.nalazek.githubsearch.data.ResultObjects.SearchResult;
+import pl.nalazek.githubsearch.data.ResultObjects.UserSearchResult;
 
-public class UserDetailedActivity extends AppCompatActivity {
+
+
+public class UserDetailedActivity extends AppCompatActivity implements UserDetailedContract.Showable {
+
+    private UserDetailsListAdapter userDetailsListAdapter;
+    private UserDetailedContract.UserActionListener userActionListener;
+    private LinearLayout progressBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        userActionListener = new UserDetailsPresenter(Injection.provideGitHubRepository(), this);
+
         setContentView(R.layout.activity_user_detailed);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        String userUrl = getIntent().getStringExtra("userUrl");
-        String starredUrl = getIntent().getStringExtra("starredUrl");
-        starredUrl = starredUrl.substring(0,starredUrl.indexOf('{'));
-        String avatarUrl = getIntent().getStringExtra("avatarUrl");
+        configureViewElements();
 
-        LinearLayout progressBar = (LinearLayout) findViewById(R.id.include_progress);
-        UserDetailsListAdapter userDetailsListAdapter = new UserDetailsListAdapter(this, R.layout.item_user_detailed, new ArrayList<Result>(), progressBar);
+        handleIntent(getIntent());
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+
+
+    @Override
+    public void showResults(List<? extends Result> resultsArray) {
+        progressBar.setVisibility(View.INVISIBLE);
+        userDetailsListAdapter.clear();
+        userDetailsListAdapter.addAll(resultsArray);
+    }
+
+
+
+    @Override
+    public void showError(String error) {
+        userDetailsListAdapter.clear();
+        progressBar.setVisibility(View.INVISIBLE);
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    @Override
+    public void showBusy() {
+        userDetailsListAdapter.clear();
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+
+
+
+    private void configureViewElements() {
+
+        progressBar = (LinearLayout) findViewById(R.id.include_progress);
+        userDetailsListAdapter = new UserDetailsListAdapter(
+                this,
+                R.layout.item_user_detailed,
+                new ArrayList<Result>());
 
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(userDetailsListAdapter);
+    }
 
-        Query[] queries;
-        QueryBuilder queryBuilder = new QueryBuilder();
 
-        userDetailsListAdapter.showBusy();
-        try {
-            queries = queryBuilder.buildUserDetailedQuerry().build(new URL(userUrl), new URL(starredUrl), null, userDetailsListAdapter);
-            QueryTask queryTask = new QueryTask();
-            queryTask.execute(queries);
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            SearchResult searchResult = intent.getParcelableExtra(UserSearchResult.PARCELABLE_TAG);
+            userActionListener.requestDetailedData(searchResult);
         }
-        catch (MalformedURLException e) {
-            userDetailsListAdapter.showError(e.getMessage());
-        }
-
     }
 }
